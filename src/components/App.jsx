c4719e9dd4d898ec808f0d707f87c3f256cd7319
Carlos Pretelin <carlosppretelin@gmail.com>
2022-11-24 08:09:56 -0600

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'
+    >Hla mundo mora</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+    <App />
+    </StrictMode>
+);
+
