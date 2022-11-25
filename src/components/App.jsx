d978f1b45400699cf0f3d873f8efa2f452d8bc43
Carlos Pretelin <carlosppretelin@gmail.com>
2022-11-25 06:36:47 -0600

    Added the Login Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Login from '../containers/Login'
 
 const App = () => {
   return (
-    <div className='App'
-    >Hla mundo mora</div>
+    <div className='App'>
+   Hla mundo mora
+   <Login></Login>
+    </div>
   )
 }
 
