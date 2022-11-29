0fcefff95f8ce113bee21ade04d1cfa2ca977837
Carlos Pretelin <carlosppretelin@gmail.com>
2022-11-29 03:11:14 -0600

    Added axios dependencie and a custom hook to fecth some data

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "../styles/ProductList.scss"
 import ProductItem from "../components/ProductItem"
+import axios from "axios"
+import useGetProducts from '../hooks/useGetProducts';
+
+
+const API = 'https://api.escuelajs.co/api/v1/products?limit=14&offset=1';
 
 const ProductList = () => {
+
+	const products = useGetProducts(API)
+
+	// const getData = async ()=> {
+	// 	const response = await axios.get(API);
+	// 	setProducts(response.data)
+	// 	console.log(response);
+	// }
+
+	// useEffect(async () => {
+	// 	getData
+	// }, [])
+
+
   return (
     <section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product =>(
+					<ProductItem key={product.id} />
+				))}
+				
 			</div>
 		</section>
   )
