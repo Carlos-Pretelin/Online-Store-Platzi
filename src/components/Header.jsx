6094a1727237b17daa1b7ca11a0441533fc75af5
Carlos Pretelin <carlosppretelin@gmail.com>
2022-11-30 08:09:13 -0600

    cart functionality of adding items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "../styles/Header.scss"
+import AppContext from '../context/AppContext';
 import Menu from "../components/Menu"
 import menu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
@@ -8,6 +9,7 @@ import shoppingCart from "@icons/icon_shopping_cart.svg";
 const Header = () => {
 
   const [ toggle, setToggle ] = useState(false);
+  const {state} = useContext(AppContext);
 
   const handleToggle = () =>{
     setToggle(!toggle);
@@ -49,7 +51,7 @@ const Header = () => {
         <li className="navbar-email" onClick={handleToggle}>carlosppretelin@gmail.com</li>
         <li className="navbar-shopping-cart">
           <img src={shoppingCart} alt="shopping cart" />
-          <div>2</div>
+          {state.cart.length > 0 ? <div> {state.cart.length} </div>  : null}
         </li>
       </ul>
     </div>
